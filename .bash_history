cb4977e1b609456719d72e11fa409f12fb347e3b
Alan1 <alan1@pop-os.localdomain>
2023-01-21 20:29:06 +0000

    Added pathogen to zsh and Coc to vim

---
+++ .bash_history
@@ -0,0 +1,38 @@
+sudo apt update && sudo apt upgrade -y
+vim
+sudo apt install vim
+htop
+sudo apt install htop
+sudo apt search bashtop
+sudo apt install btop
+btop
+tmux
+sudo apt install tmux
+sudo apt install build-essential
+tmux
+sudo apt install qemu-guest-agent
+systemctl start qemu-guest-agent.service 
+sudo apt install ssh
+sudo apt update
+sudo apt list --upgradeable
+sudo apt upgrade -y
+vim
+tmux
+fzf
+sudo apt update
+sudo shutdown now
+echo $SHELL
+which zsh
+echo $SHELL
+chsh
+sudo apt update
+ls
+mkdir workspace
+cd workspace/
+cd
+sudo apt install zsh
+chsh 
+tmux
+chsh 
+tmux
+source .zshrc
---
+++ .zsh_plugins.zsh
@@ -0,0 +1,8 @@
+fpath+=( /home/alan1/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source /home/alan1/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /home/alan1/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-TwoPizza9621536-SLASH-zsh-exa )
+source /home/alan1/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-TwoPizza9621536-SLASH-zsh-exa/exa.plugin.zsh
+fpath+=( /home/alan1/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-jeffreytse-SLASH-zsh-vi-mode )
+source /home/alan1/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-jeffreytse-SLASH-zsh-vi-mode/zsh-vi-mode.plugin.zsh
+fpath+=( /home/alan1/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /home/alan1/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
