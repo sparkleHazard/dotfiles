aee37b40a193d4e0c763bfd61b12496983f2cfa0
sparkleHazard <sparklehazard@protonmail.com>
2025-02-10 13:15:21 +0000

    Changes ls aliases to eza

--- .config/zshrc/sources/aliases.zsh
+++ .config/zshrc/sources/aliases.zsh
@@ -1,12 +1,12 @@
 alias vim=nvim
 alias cat='bat'
-alias fzf="fzf --preview 'batcat --color=always --style=numbers --line-range=:500 {}'"
+alias fzf="fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}'"
 alias dotgit='git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
 alias lazydot='lazygit --git-dir=$HOME/dotfiles --work-tree=$HOME'
-alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
-alias ll='ls -alf'
-alias la='ls -A'
-alias l='ls -CF'
+alias ls='eza'
+alias ll='eza -alf'
+alias la='eza -A'
+alias l='eza -CF'
